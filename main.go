99ccc4603104504a25736684dde97a3cf19bc90b
emmadal <emmanueldalougou@gmail.com>
2025-03-13 17:16:59 +0000

    Chore dependencies (#1)
    * chore: install project dependencies
    
    * feat: add command to initialize module
    
    * ci: add codeql actions for code scan
    
    * refactor: refactor release action

---
+++ main.go
@@ -0,0 +1,35 @@
+/*
+Copyright © 2025 Emmanuel Dalougou <emmanueldalougou@gmail.com>
+*/
+package main
+
+import (
+	"fmt"
+	"os"
+	"runtime"
+	"strings"
+
+	"github.com/emmadal/gopm/cmd"
+	"github.com/spf13/cobra"
+)
+
+// rootCmd represents the base command when called without any subcommands
+var root = &cobra.Command{
+	Use:   "gopm",
+	Short: "Fast, efficient package manager for JavaScript and TypeScript",
+	Long:  "gopm is a package manager for JavaScript and TypeScript projects.\nIt is designed to be fast, efficient and easy to use and inspired by npm and yarn.",
+	Version: strings.Join([]string{
+		"v1.0.0",
+		fmt.Sprintf("%s %s/%s", runtime.Version(), runtime.GOOS, runtime.GOARCH),
+		"https://github.com/emmadal/gopm",
+	}, "\n"),
+}
+
+func main() {
+	root.AddCommand(cmd.InitCmd)
+
+	if err := root.Execute(); err != nil {
+		fmt.Fprintf(os.Stderr, "failed to execute command: %v\n", err)
+		os.Exit(1)
+	}
+}
